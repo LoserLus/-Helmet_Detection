4504264e5b3a87cba8281ac49d5e8bcc2188132b
Loser_Lus <luyilong1314@126.com>
2023-04-14 10:33:17 +0800

    修改为本地加载模型

--- QTGUI/Detector.py
+++ QTGUI/Detector.py
@@ -5,7 +5,7 @@ from PIL import Image
 class Detector:
     def __init__(self):
         self.model = 'F:\Desktop\models\yolov5s_best.pt'
-        self.inference = torch.hub.load('ultralytics/yolov5', 'custom', path=self.model)
+        self.inference = torch.hub.load('E:\Yolo5\yolov5', 'custom', path=self.model,source='local')
 
         # self.inference = torch.load(self.model)
 
