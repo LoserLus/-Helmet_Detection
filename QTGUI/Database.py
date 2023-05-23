3117e201f4a2505e71c31a320e95b39ff4cae0c0
Loser_Lus <luyilong1314@126.com>
2023-05-23 20:43:56 +0800

    增加功能,用于支持数据导出

--- QTGUI/Database.py
+++ QTGUI/Database.py
@@ -1,12 +1,17 @@
+import datetime  # 依赖
 import pandas as pd
 from sqlalchemy import create_engine
-import datetime  # 依赖
+from sqlalchemy.orm import sessionmaker
+
+from QTGUI.ORMClass import VideoClass, DataClass
 
 
 class Database:
-    def __init__(self):
+    def __init__(self, echo=False):
         self.engine = create_engine(
-            "mysql+pymysql://{}:{}@{}:{}/{}".format('root', '123456', 'localhost', '3306', 'detection'))
+            "mysql+pymysql://{}:{}@{}:{}/{}".format('root', '123456', 'localhost', '3306', 'detection'), echo=echo)
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
 
     def insert(self, name, helmet, head):
         data_time = datetime.datetime.now().strftime("%Y-%m-%d%H:%M:%S")  # 系统时间
@@ -19,9 +24,33 @@ class Database:
         df_write.to_sql('result', self.engine, index=False, if_exists='append')
         return True
 
+    def insertData(self, datalist):
+        self.session.add_all(datalist)
+        self.session.commit()
+
+    def insertResult(self, result):
+        self.session.add(result)
+        self.session.flush()
+        self.session.commit()
+        return result.id
+
+    def queryAllResult(self):
+        return self.session.query(VideoClass).all()
+
+    def queryResultByIds(self, ids):
+        return self.session.query(VideoClass).filter(VideoClass.id.in_(ids)).statement
+
+    def queryDataById(self, vid):
+        return self.session.query(DataClass).filter(DataClass.vid == vid).statement
+
+    def updateResultCount(self, result):
+        self.session.query(VideoClass).filter(VideoClass.id == result.id).update(
+            {"helmet": result.helmet, "head": result.head, "total": result.total})
+        self.session.commit()
+
     def query2Excel(self, fileName):
         sql_query = 'select * from result;'
         # 使用pandas的read_sql_query函数执行SQL语句，并存入DataFrame
         df_read = pd.read_sql_query(sql_query, self.engine)
-        df_read.to_excel('./data/'+fileName + '.xlsx', index=False)
+        df_read.to_excel('./data/' + fileName + '.xlsx', index=False)
         return True
